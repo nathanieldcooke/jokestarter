ac064bdd581311b85029437c2982d06cb42d23aa
nathanieldcooke <nathanield.cooke@gmail.com>
2021-12-25 11:08:55 -0800

    add drawn green lines to background of site

--- src/frontend-src/src/components/Main.tsx
+++ src/frontend-src/src/components/Main.tsx
@@ -28,6 +28,11 @@ const Main = (props:{props: {}}|React.ReactNode) => {
             path="/contributions/page/:pageNumber"
           ><div>Contributions and Page</div></Route>
         </Switch>
+      <div id='background-lines' >
+        <img id='left-img' src='https://cdn.optimizely.com/img/14069890047/72ae3620b85d48c1878cbe4d0866665d.png'/>
+        <img id='right-img' src='https://cdn.optimizely.com/img/14069890047/efeeb04eb14c4a70a1b3ac360ea795d2.png'/>
+        <img id='bottom-img' src='https://ksr-static.imgix.net/c51lnrg9-doodle_continue.png?ixlib=rb-2.1.0&auto=compress%2Cformat&w=1000&fit=min&s=dc34091fa7d24f5d676e0e0201337f9b'/>
+      </div>
     </div>
     );
   }
