fc8c3f763e3d02303f3761eb66499a4cd1d7855a
nathanieldcooke <nathanield.cooke@gmail.com>
2021-12-31 07:56:19 -0800

    fix page number reset when logging out

--- dist/frontend/src/components/Main.js
+++ dist/frontend/src/components/Main.js
@@ -3,13 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = __importDefault(require("react"));
+var react_redux_1 = require("react-redux");
 var react_router_dom_1 = require("react-router-dom");
 require("../compStyles/Main.css");
 var Contributions_1 = __importDefault(require("./Contributions"));
 var Project_1 = __importDefault(require("./Project"));
 var Projects_1 = __importDefault(require("./Projects"));
 var Main = function () {
-    return (react_1.default.createElement("div", { id: 'main' },
+    var sessionUser = (0, react_redux_1.useSelector)(function (state) { return state.session; });
+    return (react_1.default.createElement("div", { id: 'main', key: sessionUser.user.id },
         react_1.default.createElement(react_router_dom_1.Switch, null,
             react_1.default.createElement(react_router_dom_1.Route, { path: "/category/:categoryName/page/:pageNumber" },
                 react_1.default.createElement(Projects_1.default, null)),
